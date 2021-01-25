fcbc820bef0b4c55212b8def29c7a86ade9e6ccc
eddyalfaro <ehalfaroc@ou.edu>
2021-01-25 17:27:28 -0600

    Opening the test for Density

---
+++ tests/properties/derived/DensityTest.java
@@ -0,0 +1,64 @@
+package properties.derived;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DensityTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testAddSIFactor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveSIFactor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetAtDoubleMassUnitsVolumeUnits() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetAtDoubleMassUnitsVolumeUnitsSIUnitsSIUnits() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSetAtDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCalculate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetValueInMassUnitsVolumeUnits() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testReplaceSIFactor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
